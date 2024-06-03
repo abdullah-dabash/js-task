ea8502d15b4a4abbc7b638408f6ce931fa2750b9
abdullah-dabash <abdullah.dabash99@gmail.com>
2024-06-03 20:23:50 +0300

    q2+q3

---
+++ app.js
@@ -0,0 +1,98 @@
+// ---------------------Q1---------------------------------------
+// console.log(-5 * 3); 
+
+// console.log("JavaScript " + 50); 
+
+// console.log(17 % 5); 
+
+// console.log(5 % 17); 
+
+// console.log(5 / 10); 
+
+// console.log(4 === '4'); 
+
+// console.log(4 != 5);
+
+// console.log(7 <= 8); 
+
+// console.log("Hello" + 5); 
+
+// console.log(Math.ceil(x) - Math.floor(x)); 
+
+// console.log(Math.pow(x, 2)); 
+// ---------------------Q2---------------------------------------
+// let msg = prompt("input a number")
+//     alert("msg");
+//     console.log(msg)
+// ---------------------Q3---------------------------------------
+// let number1 = prompt("Enter the first number:");
+// let number2 = prompt("Enter the second number:");
+
+//     if (number1 < number2) {
+//         console.log("Numbers in ascending order: " + number1 + ", " + number2);
+//     } else if (number1 > number2) {
+//         console.log("Numbers in ascending order: " + number2 + ", " + number1);
+//     } else {
+//         console.log("Numbers are equal: " + number1 + ", " + number2);
+//     }
+// ---------------------Q4---------------------------------------
+// let number1 = prompt("Enter the first number:");
+// let number2 = prompt("Enter the second number:");
+
+//     if (number1 < number2) {
+//         console.log("the larger number " + number2);
+//         alert(number2)
+//     } else if (number1 > number2) {
+//         console.log("the larger number : " + number1);
+//         alert(number1)
+//     } 
+    // ---------------------Q5---------------------------------------       
+// let number1 = parseFloat(prompt("Enter the first number:"));
+// let number2 = parseFloat(prompt("Enter the second number:"));
+
+// let sum = number1 + number2;
+
+// alert("The sum of " + number1 + " and " + number2 + " is: " + sum);
+// Assuming you have a variable named "number" containing the number to be evaluated
+
+// Example:
+let number = 9;
+
+switch(number) {
+    case 1:
+        console.log("ONE");
+        break;
+    case 2:
+        console.log("TWO");
+        break;
+    case 3:
+        console.log("THREE");
+        break;
+    case 4:
+        console.log("FOUR");
+        break;
+    case 5:
+        console.log("FIVE");
+        break;
+    case 6:
+        console.log("SIX");
+        break;
+    case 7:
+        console.log("SEVEN");
+        break;
+    case 8:
+        console.log("EIGHT");
+        break;
+    case 9:
+        console.log("NINE");
+        break;
+    default:
+        console.log("PLEASE TRY AGAIN");
+        break;
+}
+
+    
+    
+    
+
+
