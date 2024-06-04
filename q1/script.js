16f21ae2549894cf6ba1295f5bec8491c42a1b8a
PotteryBarn <progithub.p1@gmail.com>
2024-06-05 02:14:11 +0300

    done

--- q1/script.js
+++ q1/script.js
@@ -1,24 +1,32 @@
 let name = prompt("Please enter your name:");
 console.log("Hello, " + name + "!");
-let gender = prompt("enter your gender");
-console.log(gender);
-let x;
-if (gender == "male") {
-  x = "mr.";
-} else if (gender == "female") {
-  x = "ms.";
+
+while (true) {
+  var gender = prompt("enter your gender");
+  console.log(gender);
+  var x;
+  if (gender === "male") {
+    x = "mr.";
+    alert("welcome " + x + " " + name);
+    break;
+  } else if (gender === "female") {
+    x = "ms.";
+    alert("welcome " + x + " " + name);
+    break;
+  } else {
+    alert("gender should be male or female");
+    x = "";
+  }
 }
-else{
-    alert ("gender should be male or female")
-    x ="";
+
+let order = prompt("what do you want to order(dount/bakery/coffee/ice cream)?");
+console.log(order);
+alert("your order is being prepared :)");
+console.log(x + name + " " + order);
+const data = [name, gender, order];
+let m;
+console.log(data);
+for (let i = 0; i < data.length; i++) {
+  m = m = data[i];
+  console.log(m);
 }
-
-
-alert("welcome " + x + " " + name);
-
-let order = prompt("what do you want to order(dount/bakery/coffee/ice cream)?")
-console.log(order)
-alert ("your order is being prepared :)")
-console.log(x +name+ " " + order )
-
-
--- q2+q3/app.js
+++ q2+q3/app.js
@@ -17,9 +17,9 @@
 
 // console.log("Hello" + 5); 
 
-// console.log(Math.ceil(x) - Math.floor(x)); 
+// console.log(Math.ceil(8.7) - Math.floor(5.1)); 
 
-// console.log(Math.pow(x, 2)); 
+// console.log(Math.pow(8, 2)); 
 // ---------------------Q2---------------------------------------
 // let msg = prompt("input a number")
 //     alert("msg");
@@ -46,53 +46,85 @@
 //         console.log("the larger number : " + number1);
 //         alert(number1)
 //     } 
-    // ---------------------Q5---------------------------------------       
-// let number1 = parseFloat(prompt("Enter the first number:"));
-// let number2 = parseFloat(prompt("Enter the second number:"));
+// ---------------------Q5---------------------------------------       
+// let number1 = prompt("Enter the first number:");
+// let number2 = prompt("Enter the second number:");
+// let num1=parseFloat(number1);
+// let num2=parseFloat(number2);
 
-// let sum = number1 + number2;
+// if(num1 && num2){
+//     let sum = num1 + num2 ;
+//     alert(sum);
+// }
+// ---------------------Q6---------------------------------------       
+// let number = 9;
 
-// alert("The sum of " + number1 + " and " + number2 + " is: " + sum);
-// Assuming you have a variable named "number" containing the number to be evaluated
+// switch(number) {
+//     case 1:
+//         console.log("ONE");
+//         break;
+//     case 2:
+//         console.log("TWO");
+//         break;
+//     case 3:
+//         console.log("THREE");
+//         break;
+//     case 4:
+//         console.log("FOUR");
+//         break;
+//     case 5:
+//         console.log("FIVE");
+//         break;
+//     case 6:
+//         console.log("SIX");
+//         break;
+//     case 7:
+//         console.log("SEVEN");
+//         break;
+//     case 8:
+//         console.log("EIGHT");
+//         break;
+//     case 9:
+//         console.log("NINE");
+//         break;
+//     default:
+//         console.log("PLEASE TRY AGAIN");
+//         break;
+// }
+// ---------------------Q7---------------------------------------       
+//     var m;
+//    var numbers =  [0,1,2,3,4,5]
+//    for (let i = 0; i < numbers.length;i++) {
+//     m =  numbers[i];
+//     alert(m)
+//   }
+// ---------------------Q8---------------------------------------       
+// let num=[]
+// for (let i=0; i<=5; i++){
+//     num += [i]
+// }
+// alert(num)
+// -------------q9------------
+// for (let i = 0; i <= 100; i++) {
+//     if(i %3 === 0){console.log(i)}
+// }
+// -------------q10------------
 
-// Example:
-let number = 9;
+// while (true) {
+//     number = prompt("Please enter a number between 0 and 100:");
 
-switch(number) {
-    case 1:
-        console.log("ONE");
-        break;
-    case 2:
-        console.log("TWO");
-        break;
-    case 3:
-        console.log("THREE");
-        break;
-    case 4:
-        console.log("FOUR");
-        break;
-    case 5:
-        console.log("FIVE");
-        break;
-    case 6:
-        console.log("SIX");
-        break;
-    case 7:
-        console.log("SEVEN");
-        break;
-    case 8:
-        console.log("EIGHT");
-        break;
-    case 9:
-        console.log("NINE");
-        break;
-    default:
-        console.log("PLEASE TRY AGAIN");
-        break;
-}
-
-    
-    
-    
-
+//     if (number > -1 && number < 101) {
+//         alert(true);
+//         break;
+//     } else {
+//         alert("Error: Please enter a valid number between 0 and 100.");
+//     }
+// }
+// -------------q11------------
+// let number = prompt("Please enter a number:");
+// let sum = 0;
+// for (let i = 0; i <= number; i++) {
+//     sum += i;
+// }
+// alert("The sum of numbers from 0 to " + number + " is: " + sum);
 
